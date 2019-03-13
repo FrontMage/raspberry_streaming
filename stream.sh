4a62d9b2f5cc049e0e4753e863c94f260c6c2e27
FrontMage <xbgxwh@outlook.com>
2019-03-13 11:40:15 +0800

    more fps

--- stream.sh
+++ stream.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-raspivid -vf -n -w 640 -h 480 -o - -t 0 -b 2000000 -fps 10 | nc -v 10.168.7.128 5777
+raspivid -vf -n -w 640 -h 480 -o - -t 0 -b 2000000 -fps 20 | nc -v 10.168.7.128 5777
